6d6dcd48e316c0e70ddffc3cfeda78059a56a8e3
nasamatsumoto <labrador1592mary@gmail.com>
2017-04-09 10:28:47 +0000

    change

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,7 +1,10 @@
 class RankingController < ApplicationController
     def have
+       @have = Item.find(Have.group(:item_id).order('count(item_id) desc').limit(10).pluck(:item_id))
     end
 
     def want
     end
+    
+   
 end
