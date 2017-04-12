f5acd063a222c9397a3936add5a1d55d56afd115
nasamatsumoto <labrador1592mary@gmail.com>
2017-04-12 09:18:07 +0000

    ranking

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,12 +1,15 @@
 class RankingController < ApplicationController
-    def have
-       @have = Item.find(Have.group(:item_id).order('count(item_id) desc').limit(10).pluck(:item_id))
-    end
+  def have
+    @title = "Haveランキング"
+    item_ids = Have.group(:item_id).order('count(item_id) desc').limit(10).pluck(:item_id) 
+    @items = Item.find(item_ids).sort_by{ |o| item_ids.index(o.id) }
+    render :ranking
+  end
 
-    def want
-        @want = Item.find(Want.group(:item_id).order('count(item_id) desc').limit(10).pluck(:item_id))
-    end
-    
-    
-   
+  def want
+    @title = "Wantランキング"
+    item_ids = Want.group(:item_id).order('count(item_id) desc').limit(10).pluck(:item_id) 
+    @items = Item.find(item_ids).sort_by{ |o| item_ids.index(o.id) }
+    render :ranking
+  end
 end
